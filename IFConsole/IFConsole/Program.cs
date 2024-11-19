1f501ec5b997654545c15a230cfa96be29e7c82d
Roman <81181606+romamaslov200@users.noreply.github.com>
2024-11-19 09:56:37 +0300

    Add files via upload

---
+++ IFConsole/IFConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFConsole
+{
+    internal class Program
+    {
+        static ProverkaType proverka = new ProverkaType();
+        static SwitchControl switchControl = new SwitchControl();
+
+        static int a;
+        static int b;
+        static void Main(string[] args)
+        {
+            start:
+            a = proverka.Type<int>("Введите целое число => ");
+            b = proverka.Type<int>("Введите целое число => ");
+
+            if (a == b) 
+            {
+                switchControl.Control(0, a, b);
+                //Console.WriteLine("Числа рвны");
+            }
+
+            else if (a < b)
+            {
+                switchControl.Control(1, a, b);
+                //Console.WriteLine($"{a} < {b}");
+            }
+
+            else if (a > b)
+            {
+                switchControl.Control(2, a, b);
+                //Console.WriteLine($"{a} > {b}");
+            }
+            Console.ReadKey();
+
+            Console.Clear();
+            goto start;
+        }
+    }
+}
---
+++ IFConsole/IFConsole/SwitchControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFConsole
+{
+    internal class SwitchControl
+    {
+        public void Control(int i, int a, int b) 
+        {
+            switch(i)
+            {
+                case 0:
+                    Console.WriteLine("Числа рвны");
+                    break;
+
+                case 1:
+                    Console.WriteLine($"{a} < {b}");
+                    break;
+
+                case 2:
+                    Console.WriteLine($"{a} > {b}");
+                    break;
+            }
+        }
+    }
+}
